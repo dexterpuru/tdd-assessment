7da7c02c58b8c878c3f04dcbedb2e7968fe79c32
Pratyaksh <pratyakshsaini@Pratyakshs-MacBook-Air.local>
2025-06-19 11:10:14 +0530

    GREEN: Added support to handle custom delimiter in StringCalculator

--- string_calculator.py
+++ string_calculator.py
@@ -1,3 +1,5 @@
+import re
+
 class StringCalculator:
     def add(self, numbers: str) -> int:
         """
@@ -12,8 +14,22 @@ class StringCalculator:
         if not numbers:
             return 0
         
-        normalized = numbers.replace('\n', ',')
-        return self._add_numbers(normalized.split(','))
+        
+        delimiter_pattern, number_string = None, None
+        
+        
+        if numbers.startswith('//'):
+            lines = numbers.split('\n', 1)
+            delimiter_pattern = lines[0][2:] # Remove '//'
+            number_string = lines[1]
+        else:
+            delimiter_pattern = ',|\n'
+            number_string = numbers
+        
+        escaped_pattern = re.escape(delimiter_pattern).replace('\\|', '|')
+        number_list = re.split(f'[{escaped_pattern}]', number_string)
+        
+        return self._add_numbers(number_list)
     
     def _add_numbers(self, number_strings: list[str]) -> int:
         """
